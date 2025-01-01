6dc87bbe432e7cc49977a49fd4aacc6ee6b76807
vishesh-sachan <visheshsachan021@gmail.com>
2025-01-01 10:00:12 +0530

    fixed build errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { WebSocketServer, WebSocket } from 'ws';
 
-const app = express()
-const httpServer = app.listen(8080)
+const app = express();
+const httpServer = app.listen(8080);
 
 const wss = new WebSocketServer({ server: httpServer });
 
@@ -10,45 +10,47 @@ enum Status {
   pending,
   approved,
   rejected,
-  closed
+  closed,
 }
 
 interface Data {
-  isStudent: Boolean,
-  passId: number,
-  studentId: number,
-  status: Status
+  isStudent: boolean;
+  passId: number;
+  studentId: number;
+  status: Status;
 }
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws: WebSocket) {
   ws.on('error', console.error);
 
-  ws.on('message', function message(message) {
+  ws.on('message', function message(message: any) {
     const data: Data = JSON.parse(message.toString());
 
     if (!data.isStudent) {
-      // console.log(`from warden ${data.isStudent},${data.passId},${data.status},${data.studentId}`);
-          wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({
-                isStudent: data.isStudent,
-                passId: data.passId,
-                studentId: data.studentId,
-                status: data.status
-              }));
-            }
-          });
-    }
-    
-    if (data.isStudent) {
-      // console.log(`from student ${data}`);
-      wss.clients.forEach(function each(client) {
+      wss.clients.forEach(function each(client: WebSocket) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({
-            isStudent: data.isStudent,
-            passId: data.passId,
-            studentId: data.studentId
-          }));
+          client.send(
+            JSON.stringify({
+              isStudent: data.isStudent,
+              passId: data.passId,
+              studentId: data.studentId,
+              status: data.status,
+            })
+          );
+        }
+      });
+    }
+
+    if (data.isStudent) {
+      wss.clients.forEach(function each(client: WebSocket) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              isStudent: data.isStudent,
+              passId: data.passId,
+              studentId: data.studentId,
+            })
+          );
         }
       });
     }
